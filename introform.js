97f3d245628f8f5a2679ccd828ac22e05aa7cbb0
Ameya Daigavane <ameya.d.98@gmail.com>
2017-11-12 14:31:04 +0530

    Add files via upload

--- introform.js
+++ introform.js
@@ -1,10 +1,40 @@
+$('body').on('change', 'input[type="file"][data-toggle="custom-file"]', function (ev) {
+
+     const $input = $(this);
+     const target = $input.data('target');
+     const $target = $(target);
+
+     if (!$target.length)
+       return console.error('Invalid target for custom file', $input);
+
+     if (!$target.attr('data-content'))
+       return console.error('Invalid `data-content` for custom file target', $input);
+
+     // set original content so we can revert if user deselects file
+     if (!$target.attr('data-original-content'))
+       $target.attr('data-original-content', $target.attr('data-content'));
+
+     const input = $input.get(0);
+
+     let name = _.isObject(input)
+       && _.isObject(input.files)
+       && _.isObject(input.files[0])
+       && _.isString(input.files[0].name) ? input.files[0].name : $input.val();
+
+     if (_.isNull(name) || name === '')
+       name = $target.attr('data-original-content');
+
+     $target.attr('data-content', name);
+
+   });
+
 function submitForm(oFormElement)
 {
-  console.log('submitted');
+     console.log('submitted');
 
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function(){ alert (xhr.responseText); }
-  xhr.open (oFormElement.method, oFormElement.action, true);
-  xhr.send (new FormData (oFormElement));
-  return false; // Markus had to return true */
+     var xhr = new XMLHttpRequest();
+     xhr.onload = function(){ alert (xhr.responseText); }
+     xhr.open (oFormElement.method, oFormElement.action, true);
+     xhr.send (new FormData (oFormElement));
+     return false; // Markus had to return true */
 }
